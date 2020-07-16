8c36e1680ad6eab7d838f76dd035cb69e63c41d0
ZHENGJIAN LIU <comezheng@gmail.com>
2020-07-16 23:54:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function sayHiToGrandma(string){
   if(string = 'I love you, Grandma.'){
     return 'I love you, too.'
   } else if(string = string.toUpperCase()){
-    return ''
+    return 'YES INDEED!'
+  } else if(string = string.toLowerCase()){
+    return
   }
 }
