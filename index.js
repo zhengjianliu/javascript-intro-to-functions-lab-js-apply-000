a9ac6567eedb5bc1fd8d5b77cc925a4327867db6
ZHENGJIAN LIU <comezheng@gmail.com>
2020-07-16 23:28:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function shout(string){
+  console.log(string.toUpperCase())
+}
+
+function whisper(string){
+  console.log(string.toLowerCase())
+}
