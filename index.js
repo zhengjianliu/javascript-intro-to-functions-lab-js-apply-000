ef0c6b7a4b86534cac016d95f6bde8f8339f461d
ZHENGJIAN LIU <comezheng@gmail.com>
2020-07-16 23:51:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(`string` = 'I love you, Grandma.'){
+  if(string = 'I love you, Grandma.'){
     return 'I love you, too.'
   }
 }
